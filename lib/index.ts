ac56893ad1b7ac4764c03ac3aa054776f52c0507
sina-byn <sina.sina.bayan@protonmail.com>
2025-07-25 11:05:12 +0330

    feat: Jdenticon exported from the lib entry to be used both by importing it or registering it as a plugin

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,13 @@
-// * lib entry point
+import type { App } from 'vue';
+
+// * components
+import Jdenticon from '../src/components/Jdenticon.vue';
+
+export { type JdenticonProps } from '../src/components/Jdenticon.vue';
+export { Jdenticon };
+
+export default {
+  install: (app: App) => {
+    app.component('Jdenticon', Jdenticon);
+  },
+};
