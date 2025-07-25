c8764f4061dfa5991feaff4575f4dd126b73cd18
sina-byn <sina.sina.bayan@protonmail.com>
2025-07-25 11:02:25 +0330

    feat: Jdenticon component added

--- src/components/Jdenticon.vue
+++ src/components/Jdenticon.vue
@@ -1,3 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as jdenticon from 'jdenticon';
 
-<template></template>
+import { watch, useTemplateRef } from 'vue';
+
+export type JdenticonProps = { size?: number };
+
+const icon = useTemplateRef<SVGElement>('icon');
+const value = defineModel<string>();
+
+const { size = 100 } = defineProps<JdenticonProps>();
+
+watch([icon, value], () => {
+  icon.value && jdenticon.update(icon.value, value.value ?? '');
+});
+</script>
+
+<template>
+  <svg ref="icon" :data-jdenticon-value="value" :width="size" :height="size" />
+</template>
