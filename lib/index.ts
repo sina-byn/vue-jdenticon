d526b852f83bf23194695969877fe39dc2afd534
sina-byn <sina.sina.bayan@protonmail.com>
2025-08-02 10:08:53 +0330

    feat: StaticJdenticon component added

--- lib/index.ts
+++ lib/index.ts
@@ -2,12 +2,15 @@ import type { App } from 'vue';
 
 // * components
 import Jdenticon from '../src/components/Jdenticon.vue';
+import StaticJdenticon from '../src/components/StaticJdenticon.vue';
 
 export { type JdenticonProps } from '../src/components/Jdenticon.vue';
-export { Jdenticon };
+export { type StaticJdenticonProps } from '../src/components/StaticJdenticon.vue';
+export { Jdenticon, StaticJdenticon };
 
 export default {
   install: (app: App) => {
     app.component('Jdenticon', Jdenticon);
+    app.component('StaticJdenticon', StaticJdenticon);
   },
 };
---
+++ src/components/StaticJdenticon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import * as jdenticon from 'jdenticon';
+
+import { watch, useTemplateRef } from 'vue';
+
+export type StaticJdenticonProps = { size?: number; value: string };
+
+const icon = useTemplateRef<SVGElement>('icon');
+
+const { size = 100, value } = defineProps<StaticJdenticonProps>();
+
+watch([icon], () => {
+  icon.value && jdenticon.update(icon.value, value ?? '');
+});
+</script>
+
+<template>
+  <svg ref="icon" :data-jdenticon-value="value" :width="size" :height="size" />
+</template>
